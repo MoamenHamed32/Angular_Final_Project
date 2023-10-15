dc361ffb4b11683521ee8c047413a63768a9c562
omar adel <elhwety5555@gmail.com>
2023-10-15 16:05:47 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesWatchListComponent } from './movies-watch-list/movies-watch-list.component';
+import { MovieCardDetailsComponent } from './movie-card-details/movie-card-details.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent,
+    MoviesListComponent,
+    HeaderComponent,
+    MoviesWatchListComponent,
+    MovieCardDetailsComponent,
+    RecommendationComponent
   ],
   imports: [
     BrowserModule,
