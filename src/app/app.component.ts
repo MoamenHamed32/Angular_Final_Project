adbc6e4ec6df6a6ff4f4d8a12369a23d7914c2ac
Moamen Hamed <moamen.hamed33322@gmail.com>
2023-10-15 15:38:02 +0200

    Adding API Methods in Service File

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MoviesRequestService } from './services/movies-request.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-Final-Project';
+  constructor(private movieReq: MoviesRequestService) {}
+  ngOnInit() {
+    this.movieReq.getMovieList().subscribe((data) => console.log(data));
+    this.movieReq
+      .getMovieDetails(575264)
+      .subscribe((data) => console.log(data));
+    this.movieReq
+      .getRecommendedMovies(575264)
+      .subscribe((data) => console.log(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
@@ -18,13 +18,10 @@ import { RecommendationComponent } from './recommendation/recommendation.compone
     HeaderComponent,
     MoviesWatchListComponent,
     MovieCardDetailsComponent,
-    RecommendationComponent
+    RecommendationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
